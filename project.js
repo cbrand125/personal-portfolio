602fd1533271726a740c0d88761822fcb590e70b
cbrand125 <cbrand@talentpath.com>
2019-09-13 10:15:07 -0700

    finish basic website

--- project.js
+++ project.js
@@ -7,20 +7,14 @@ if (urlParams.has('project')) {
   project = urlParams.get('project');
 }
 
-let projectData;
-switch (project) {
-  case 'flow':
-    projectData = allProjectData.flow;
-    break;
-
-  default:
-    document.getElementById('demoBtn').style = 'display: none;';
-    document.getElementById('codeBtn').style = 'display: none;';
-}
+const projectData = allProjectData[project];
 
 if (projectData) {
   document.getElementById('screenshot').src = projectData.image;
   document.getElementById('projDesc').innerHTML = projectData.description;
   document.getElementById('demoBtn').setAttribute('href', projectData.demoLink);
   document.getElementById('codeBtn').setAttribute('href', projectData.codeLink);
+} else {
+  document.getElementById('demoBtn').style = 'display: none;';
+  document.getElementById('codeBtn').style = 'display: none;';
 }
